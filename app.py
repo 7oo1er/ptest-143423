d2775bfc3a4d151447503728651a08835517bd5a
Alexey <graff@yandex.ru>
2021-10-14 15:57:10 +0300

    no peewee error

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class event_log(BaseModel):
 
 
 try:
-    event_log.create_table(safe=True)
+    event_log.create_table()
     print("MessageLog table created!")
 except peewee.OperationalError:
     print("MessageLog table already exists!")
