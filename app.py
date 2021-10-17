09c0a8f4de29878cf8cff9138409b6ba35853630
7oo1er <graff@yandex.ru>
2021-10-17 16:17:38 +0300

    new tests

--- app.py
+++ app.py
@@ -1,36 +1,41 @@
 import os
 import re
-from peewee import *
-#import peewee
+#from peewee import *
+import peewee
 from dotenv import load_dotenv
+from datetime import datetime
+
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
-regex = r"postgres://([\w_]{2,20}):([^@]{2,80})@([^:]{2,80}):5432/([\w_]{2,20})"
+regex = r"postgres://([\w_]{2,20}):([^@]{2,80})@([^:]{2,80}):5432/([\w_0-9]{2,20})"
 match = re.search(regex, DATABASE_URL)
 db_user, db_password, db_url, db_base = match[1], match[2], match[3], match[4]
-db = PostgresqlDatabase(user=db_user, database=db_base, password=db_password, host=db_url)
+db = peewee.PostgresqlDatabase(user=db_user, database=db_base, password=db_password, host=db_url)
 
 
-class BaseModel(Model):
+class BaseModel(peewee.Model):
     class Meta:
         database = db
 
 
 class event_log(BaseModel):
-    d_id = CharField(null=True, unique=True)
+    d_id = peewee.CharField(null=True, unique=True)
 
 
 try:
-    event_log.create_table()
+    event_log.create_table(safe=True)
     print("MessageLog table created!")
 except peewee.OperationalError:
     print("MessageLog table already exists!")
 
+now = datetime.now()
+today = now.strftime("%H%M%S")
+newrecord = (event_log.insert(d_id=today).execute())
 
 # for cls in globals().values():
-#     if type(cls) == peewee.BaseModel:
+#     if type(cls) == BaseModel:
 #         try:
 #             cls.create_table()
-#         except peewee.OperationalError as e:
+#         except OperationalError as e:
 #             print(e)
